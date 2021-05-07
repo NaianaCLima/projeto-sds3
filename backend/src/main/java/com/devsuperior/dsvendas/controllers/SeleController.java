b6349ed3195ca6e9d60045de69f2dbc4a9eb90e1
NaianaCLima <naianacordeirodelima@gmail.com>
2021-05-07 18:26:10 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SeleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SeleController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SeleDto;
+import com.devsuperior.dsvendas.dto.SeleSucessDto;
+import com.devsuperior.dsvendas.dto.SeleSumDto;
 import com.devsuperior.dsvendas.service.SeleService;
 
 @RestController
@@ -21,6 +25,18 @@ public class SeleController {
 	@GetMapping
 	public ResponseEntity<Page<SeleDto>> findAll(Pageable pageable){
 		Page<SeleDto> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SeleSumDto>> amountGroupedBySeller(){
+		List<SeleSumDto> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/sucess-by-seller")
+	public ResponseEntity<List<SeleSucessDto>> sucessGroupedBySeller(){
+		List<SeleSucessDto> list = service.sucessGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}
 
--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SeleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SeleRepository.java
@@ -1,10 +1,22 @@
 package com.devsuperior.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsvendas.dto.SeleSucessDto;
+import com.devsuperior.dsvendas.dto.SeleSumDto;
 import com.devsuperior.dsvendas.entities.Sele;
 
 public interface SeleRepository extends JpaRepository<Sele, Long> {
 	
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SeleSumDto(obj.seller, SUM(obj.amount)) "
+			+ "FROM Sele AS obj GROUP BY obj.seller")
+	List<SeleSumDto> amountGroupedBySeller();
+	
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SeleSucessDto(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ "FROM Sele AS obj GROUP BY obj.seller")
+	List<SeleSucessDto> sucessGroupedBySeller();
 
 }
