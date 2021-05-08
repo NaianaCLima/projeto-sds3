27d2080ca96bec6bfc1a9b88330afe836ceeeb5a
NaianaCLima <naianacordeirodelima@gmail.com>
2021-05-08 11:35:09 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,21 +1,40 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sele';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+} 
 
 const DonutChart = () => {
 
-    const mockData = {
+    let charData: ChartData = {labels: [], series: []};// let pode mudar o dado
+
+    axios.get(`${BASE_URL}/seles/amount-by-seller`).then(response =>{
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        charData = {labels:myLabels, series: mySeries};
+        console.log(charData);
+    });
+        
+    /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    }*/
     
-    const options = {
+    const options = { //const é constante n muda o dado
         legend: {
             show: true
         }
     }
     return (        
        <Chart 
-            options={{ ...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{ ...options, labels: charData.labels}}
+            series={charData.series}
             type="donut" /* tipo do gráfico*/
             height="240"/*altura do gráfico*/
        />
