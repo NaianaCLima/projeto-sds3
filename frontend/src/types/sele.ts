5f215fc357a0ceba1925e674b2f9c78085b8d1ca
NaianaCLima <naianacordeirodelima@gmail.com>
2021-05-09 21:53:57 -0300

    DataTable integration

--- frontend/src/types/sele.ts
+++ frontend/src/types/sele.ts
@@ -1,4 +1,27 @@
 import { type } from "node:os"
+import { Seller } from "./seller"
+
+export type Sele = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+       
+}
+
+export type SelePage = {
+    content?: Sele[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+} 
 
 export type SaleSum = {
     sellerName: string;
